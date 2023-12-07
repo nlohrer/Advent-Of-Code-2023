b51bed859fe9af4137b52708428d9d2512af25c4
apsa <n.Lohrer@campus.lmu.de>
2023-12-07 10:20:42 +0100

    Add day 7 part 2 solution

--- AdventOfCode/Day07.cs
+++ AdventOfCode/Day07.cs
@@ -3,7 +3,8 @@
     public class Day07 : BaseDay
     {
         private readonly string _input;
-        public static char[] cardTypes = ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'];
+        public static char[] cardTypesPart1 = ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'];
+        public static char[] cardTypesPart2 = ['J', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'Q', 'K', 'A'];
 
         public Day07()
         {
@@ -11,14 +12,16 @@
         }
         public override ValueTask<string> Solve_1()
         {
-            var parsed = Day07.ParseInput(_input);
-            int total = parsed.OrderBy(hand => hand.GetTotalValue()).Select((hand, index) => (index + 1) * hand.bid).Sum();
+            var parsed = ParseInput(_input);
+            int total = parsed.OrderBy(hand => hand.GetTotalValuePart1()).Select((hand, index) => (index + 1) * hand.bid).Sum();
             return new ValueTask<string>(total.ToString());
         }
 
         public override ValueTask<string> Solve_2()
         {
-            throw new NotImplementedException();
+            var parsed = ParseInput(_input);
+            int total = parsed.OrderBy(hand => hand.GetTotalValuePart2()).Select((hand, index) => (index + 1) * hand.bid).Sum();
+            return new ValueTask<string>(total.ToString());
         }
 
         public static IEnumerable<Hand> ParseInput(string input)
@@ -38,18 +41,36 @@
 
     public record Hand(string cards, int bid)
     {
-        public int[] GetAmounts()
+        public int[] GetAmounts(char[] cardTypes)
         {
-            int[] amounts = new int[Day07.cardTypes.Length];
+            int[] amounts = new int[cardTypes.Length];
             foreach (char card in cards)
             {
-                int rank = Array.IndexOf(Day07.cardTypes, card);
+                int rank = Array.IndexOf(cardTypes, card);
                 amounts[rank]++;
             }
             return amounts;
         }
+        
+        public static int GetValuePart2(int[] amounts)
+        {
+            int jokerAmount = amounts[0];
 
-        public static int GetValue(int[] amounts)
+            int max = -1;
+            int indexOfMax = -1;
+            for (int i = 0; i < amounts.Length - 1; i++)
+                if (max < amounts.Skip(1).ToArray()[i])
+                {
+                    indexOfMax = i + 1;
+                    max = amounts[i + 1];
+                }
+
+            amounts[indexOfMax] += jokerAmount;
+            amounts[0] = 0;
+            return GetValuePart1(amounts);
+        }
+
+        public static int GetValuePart1(int[] amounts)
         {
             int multiplier = 10000000;
             if (amounts.Any(amount => amount == 5))
@@ -72,13 +93,23 @@
             return 1 * multiplier;
         }
 
-        public int GetTotalValue()
+        public int GetTotalValuePart1()
         {
-            int[] amounts = this.GetAmounts();
-            int kindValue = GetValue(amounts);
+            int[] amounts = this.GetAmounts(Day07.cardTypesPart1);
+            int kindValue = GetValuePart1(amounts);
             int inherentValue = 0;
             for (int i = 0; i < cards.Length; i++)
-                inherentValue += Array.IndexOf(Day07.cardTypes, cards[i]) * (int)Math.Round(Math.Pow(14, (cards.Length - i - 1)));
+                inherentValue += Array.IndexOf(Day07.cardTypesPart1, cards[i]) * (int)Math.Round(Math.Pow(amounts.Length, (cards.Length - i - 1)));
+            return kindValue + inherentValue;
+        }
+
+         public int GetTotalValuePart2()
+        {
+            int[] amounts = this.GetAmounts(Day07.cardTypesPart2);
+            int kindValue = GetValuePart2(amounts);
+            int inherentValue = 0;
+            for (int i = 0; i < cards.Length; i++)
+                inherentValue += Array.IndexOf(Day07.cardTypesPart2, cards[i]) * (int)Math.Round(Math.Pow(amounts.Length, (cards.Length - i - 1)));
             return kindValue + inherentValue;
         }
     }
@@ -97,8 +128,16 @@
         public void Solve()
         {
             var parsed = Day07.ParseInput(_testInput);
-            int total = parsed.OrderBy(hand => hand.GetTotalValue()).Select((hand, index) => (index + 1) * hand.bid).Sum();
+            int total = parsed.OrderBy(hand => hand.GetTotalValuePart1()).Select((hand, index) => (index + 1) * hand.bid).Sum();
             Assert.Equal(6440, total);
+        }
+
+        [Fact]
+        public void Solve2()
+        {
+            var parsed = Day07.ParseInput(_testInput);
+            int total = parsed.OrderBy(hand => hand.GetTotalValuePart2()).Select((hand, index) => (index + 1) * hand.bid).Sum();
+            Assert.Equal(5905, total);
         }
     }
 }
